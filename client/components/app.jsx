4694e009a856e51752a74fe9a11f10ef14a469f5
juliehchung <juliehchung96@gmail.com>
2019-11-21 00:33:27 +0000

    Completed add to cart front end feature

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,8 +6,9 @@ import ProductDetails from './product-details';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { view: { name: 'catalog', params: {} } };
+    this.state = { view: { name: 'catalog', params: {} }, cart: [] };
     this.setView = this.setView.bind(this);
+    this.addToCart = this.addToCart.bind(this);
   }
 
   setView(name, params) {
@@ -16,16 +17,42 @@ export default class App extends React.Component {
     });
   }
 
+  getCartItems() {
+    const requestItems = new Request('/api/cart');
+    fetch(requestItems)
+      .then(res => res.json())
+      .then(cart => this.setState({ cart }))
+      .catch(error => console.error('Fetch Failed:', error));
+  }
+
+  addToCart(product) {
+    const requestCart = new Request('/api/cart');
+    const req = {
+      method: 'POST',
+      body: JSON.stringify(product),
+      headers: { 'Content-Type': 'application/json' }
+    };
+    fetch(requestCart, req)
+      .then(response => response.json())
+      .then(product => this.setState({ cart: this.state.cart.concat(product) }))
+      .catch(error => console.error('Fetch Failed:', error));
+  }
+
+  componentDidMount() {
+    this.getCartItems();
+  }
+
   render() {
     let productElem;
     if (this.state.view.name === 'catalog') {
       productElem = <ProductList viewData={this.setView} />;
     } else if (this.state.view.name === 'details') {
-      productElem = <ProductDetails productData={this.state.view.params} viewData={this.setView} />;
+      productElem = <ProductDetails productData={this.state.view.params} viewData={this.setView} addItem={this.addToCart} />;
     }
+    const cartItemCount = this.state.cart.length;
     return (
       <>
-        <Header />
+        <Header cartItemCount={cartItemCount}/>
         <div className="container">
           {productElem}
         </div>
