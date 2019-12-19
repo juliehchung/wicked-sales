ee2177ac90354bca38cb1c2f4b0c0b50e1f2835e
juliehchung <juliehchung96@gmail.com>
2019-12-19 18:43:09 +0000

    Fixed styling for cart items responsiveness

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -5,10 +5,10 @@ function CartSummaryItem(props) {
   let price = cartItemInfo.price;
   price = '$' + ((price / 100).toFixed(2));
   return (
-    <div className="bg-white border rounded py-3 col-12 my-3">
-      <div className="row m-1">
+    <div className="bg-white border rounded col-12 py-2 my-3">
+      <div className="row py-2">
         <img src={cartItemInfo.image} alt={cartItemInfo.name} className="cart-item img-fluid col-md-6" />
-        <div className="col-md-6 d-flex flex-column align-self-center">
+        <div className="col-md-4 d-flex flex-column align-self-center">
           <h5>{cartItemInfo.name}</h5>
           <h6 className="text-muted">{price}</h6>
           <p>{cartItemInfo.shortDescription}</p>
--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -1,44 +1,41 @@
 import React from 'react';
 import CartSummaryItem from './cart-summary-item';
 
-class CartSummary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isDisabled: true
-    };
-  }
-
-  componentDidMount() {
-    this.setState({ isDisabled: !this.state.isDisabled });
-  }
-
-  render() {
-    const cart = this.props.cartItems;
-    const changeView = this.props.viewData;
-    let totalPrice = 0;
-    cart.map(product => {
-      totalPrice = totalPrice + product.price;
-    });
-    const total = '$' + ((totalPrice / 100).toFixed(2));
+function CartSummary(props) {
+  const cart = props.cartItems;
+  const changeView = props.viewData;
+  let totalPrice = 0;
+  cart.map(product => {
+    totalPrice = totalPrice + product.price;
+  });
+  const total = '$' + ((totalPrice / 100).toFixed(2));
+  if (cart.length === 0) {
     return (
       <div className="container col-10 my-5">
         <div className="back text-muted my-3" onClick={() => changeView('catalog', {})}>
           {'< Back to Catalog'}
         </div>
-        <h2>My Cart</h2>
-        <div className="d-flex flex-wrap">
-          {cart.map(cartItem => <CartSummaryItem key={cartItem.cartItemId} cartData={cartItem} />)}
-        </div>
-        <div className="container">
-          <div className="row d-flex justify-content-between align-items-center my-3">
-            <h4>Total: {total}</h4>
-            <button disabled={this.state.isDisabled} className="btn btn-primary align-self-end" onClick={() => changeView('checkout', {})}>Checkout</button>
-          </div>
-        </div>
+        <h2>Your cart is empty!</h2>
       </div>
     );
   }
+  return (
+    <div className="container col-10 my-5">
+      <div className="back text-muted my-3" onClick={() => changeView('catalog', {})}>
+        {'< Back to Catalog'}
+      </div>
+      <h2>My Cart</h2>
+      <div className="d-flex flex-wrap">
+        {cart.map(cartItem => <CartSummaryItem key={cartItem.cartItemId} cartData={cartItem} />)}
+      </div>
+      <div className="container">
+        <div className="row d-flex justify-content-between align-items-center my-3">
+          <h4>Total: {total}</h4>
+          <button className="btn btn-primary align-self-end" onClick={() => changeView('checkout', {})}>Checkout</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CartSummary;
