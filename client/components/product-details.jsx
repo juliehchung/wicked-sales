9c702ff31e32939f220e9b0505f6e75f29808ae0
juliehchung <juliehchung96@gmail.com>
2019-12-19 01:47:55 +0000

    Continuing to refactor responsive product details page, fixing text responsiveness

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -24,23 +24,23 @@ class ProductDetails extends React.Component {
     let price = productInfo.price;
     price = '$' + ((price / 100).toFixed(2));
     return (
-      <div className="container col-10 my-5">
-        <div className="">
-          <div className="text-muted m-3" onClick={() => this.props.viewData('catalog', {})}>
+      <div className="container my-5">
+        <div className="bg-white border rounded py-3">
+          <div className="text-muted ml-3" onClick={() => this.props.viewData('catalog', {})}>
             {'< Back to Catalog'}
           </div>
-          <div className="">
-            <div className="col-md-4">
+          <div className="container row m-3">
+            <div className="col-md-6">
               <img src={productInfo.image} alt={productInfo.name} className="detailsImg img-fluid rounded" />
             </div>
-            <div className="col-md-8">
+            <div className="col-md-6">
               <h5>{productInfo.name}</h5>
               <h6 className="text-muted">{price}</h6>
               <p>{productInfo.shortDescription}</p>
               <button className="btn btn-primary" onClick={() => addToCart(productInfo)}>Add to Cart</button>
             </div>
           </div>
-          <div className="container">
+          <div className="container my-4">
             {productInfo.longDescription}
           </div>
         </div>
