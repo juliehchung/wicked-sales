f92fed9f1498e9aacb258f6f7995ff88ee13a5fb
juliehchung <juliehchung96@gmail.com>
2019-12-19 19:37:02 +0000

    Disabled place order button unless user fills out all input fields

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -6,18 +6,44 @@ class CheckoutForm extends React.Component {
     this.state = {
       name: '',
       creditCard: '',
-      shippingAddress: ''
+      shippingAddress: '',
+      isDisabled: true
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
     this.setState({ [event.target.name]: event.target.value });
+    const name = this.state.name;
+    const creditCard = this.state.creditCard;
+    const shippingAddress = this.state.shippingAddress;
+    this.setDisabled(name, creditCard, shippingAddress);
+  }
+
+  setDisabled(name, creditCard, shippingAddress) {
+    if (name !== '' && creditCard !== '' && shippingAddress !== '') {
+      this.setState({ isDisabled: false });
+    } else {
+      this.setState({ isDisabled: true });
+    }
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    this.props.checkout(this.state);
+    const name = this.state.name;
+    const creditCard = this.state.creditCard;
+    const shippingAddress = this.state.shippingAddress;
+    this.props.checkout({ name, creditCard, shippingAddress });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.name !== prevState.name) {
+      this.setDisabled(this.state.name, this.state.creditCard, this.state.shippingAddress);
+    } else if (this.state.creditCard !== prevState.creditCard) {
+      this.setDisabled(this.state.name, this.state.creditCard, this.state.shippingAddress);
+    } else if (this.state.shippingAddress !== prevState.shippingAddress) {
+      this.setDisabled(this.state.name, this.state.creditCard, this.state.shippingAddress);
+    }
   }
 
   render() {
@@ -33,7 +59,7 @@ class CheckoutForm extends React.Component {
         <h2 className="container ml-4">My Cart</h2>
         <h5 className="container back text-muted ml-4">Order Total: {total}</h5>
         <div className="container d-flex flex-wrap m-3">
-          <form className="col-xl-10 ml-5" onSubmit={() => this.handleSubmit()}>
+          <form className="col-xl-10 ml-5" onSubmit={this.handleSubmit}>
             <div className="form-group">
               <label>Name</label>
               <input type="text" className="form-control" value={this.state.name} name='name' onChange={this.handleChange} />
@@ -50,7 +76,7 @@ class CheckoutForm extends React.Component {
               <div className="back text-muted mt-4" onClick={() => viewCatalog('catalog', {})}>
                 {'< Continue Shopping'}
               </div>
-              <button type="submit" className="btn btn-primary mt-3">Submit</button>
+              <button disabled={this.state.isDisabled} type="submit" className="btn btn-primary mt-3">Submit</button>
             </div>
           </form>
         </div>
