cb77c8b57dad64d4f1d60aa987259d526b12010d
juliehchung <juliehchung96@gmail.com>
2019-12-19 02:09:40 +0000

    Fixed responsiveness for product details page

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -24,24 +24,26 @@ class ProductDetails extends React.Component {
     let price = productInfo.price;
     price = '$' + ((price / 100).toFixed(2));
     return (
-      <div className="container my-5">
+      <div className="container col-10 my-5">
         <div className="bg-white border rounded py-3">
-          <div className="text-muted ml-3" onClick={() => this.props.viewData('catalog', {})}>
+          <div className="back text-muted ml-3" onClick={() => this.props.viewData('catalog', {})}>
             {'< Back to Catalog'}
           </div>
-          <div className="container row m-3">
-            <div className="col-md-6">
-              <img src={productInfo.image} alt={productInfo.name} className="detailsImg img-fluid rounded" />
+          <div className="row m-4">
+            <div className="col-md-4">
+              <img src={productInfo.image} alt={productInfo.name} className="img-fluid rounded" />
             </div>
-            <div className="col-md-6">
+            <div className="col-md-8 d-flex flex-column justify-content-end">
               <h5>{productInfo.name}</h5>
               <h6 className="text-muted">{price}</h6>
               <p>{productInfo.shortDescription}</p>
-              <button className="btn btn-primary" onClick={() => addToCart(productInfo)}>Add to Cart</button>
+              <button className="btn btn-primary align-self-start" onClick={() => addToCart(productInfo)}>Add to Cart</button>
             </div>
           </div>
-          <div className="container my-4">
-            {productInfo.longDescription}
+          <div className="row m-4">
+            <div className="col-12">
+              {productInfo.longDescription}
+            </div>
           </div>
         </div>
       </div>
