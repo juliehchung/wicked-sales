c94481e971f253f6104f8556d0aa28be8086c0c4
juliehchung <juliehchung96@gmail.com>
2019-12-23 22:34:20 +0000

    Completed styling for intro modal

--- client/components/live-demo-modal.jsx
+++ client/components/live-demo-modal.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function LiveDemoModal(props) {
   return (
     <div className="backdrop position-fixed d-flex h-100 w-100">
-      <div className="modal-contents m-auto bg-white border rounded mt-5">
+      <div className="modal-contents m-auto bg-white border rounded mt-5 p-3">
         <h2 className="m-2">Welcome!</h2>
         <p className="m-2">This is a live demo website.</p>
         <form onSubmit={event => {
@@ -11,11 +11,11 @@ function LiveDemoModal(props) {
           props.close();
         }}>
           <div className="form-check m-2">
-            <input className="form-check-input" type="checkbox" id="acknowledgement" />
+            <input className="form-check-input" type="checkbox" id="acknowledgement" required />
             <label className="form-check-label" htmlFor="acknowledgement">I acknowledge that these merchandise are not for purchase.</label>
           </div>
-          <div className="btn-group">
-            <button className="btn my-3">I Understand</button>
+          <div className="btn-group w-100 p-1">
+            <button className="btn btn-warning w-100">I Understand</button>
           </div>
         </form>
       </div>
