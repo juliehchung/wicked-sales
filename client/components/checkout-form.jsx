9d03d31d4951da25835e4953fd2c7ba36fc144f1
juliehchung <juliehchung96@gmail.com>
2019-12-26 23:37:34 +0000

    Completed front end form validation for placing an order

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -14,7 +14,8 @@ class CheckoutForm extends React.Component {
       zip: '',
       cardHolder: '',
       card: '',
-      expiration: '',
+      month: '',
+      year: '',
       cvv: '',
       checkValidity: {
         fullName: true,
@@ -28,8 +29,7 @@ class CheckoutForm extends React.Component {
         cardHolder: true,
         card: true,
         expiration: true,
-        cvv: true,
-        acknowledgement: true
+        cvv: true
       }
     };
     this.handleChange = this.handleChange.bind(this);
@@ -49,13 +49,11 @@ class CheckoutForm extends React.Component {
       cardHolder: true,
       card: true,
       expiration: true,
-      cvv: true,
-      acknowledgement: true
+      cvv: true
     };
 
     const numRegex = RegExp(/^[0-9]*$/);
     const zipRegex = RegExp(/^[0-9]*-*[0-9]*$/);
-    const expRegex = RegExp(/^[0-9]*\/*[0-9]*$/);
 
     switch (event.target.name) {
       case 'phone':
@@ -67,11 +65,6 @@ class CheckoutForm extends React.Component {
         break;
       case 'zip':
         if (zipRegex.test(event.target.value)) {
-          this.setState({ [event.target.name]: event.target.value });
-        }
-        break;
-      case 'expiration':
-        if (expRegex.test(event.target.value)) {
           this.setState({ [event.target.name]: event.target.value });
         }
         break;
@@ -98,13 +91,11 @@ class CheckoutForm extends React.Component {
       cardHolder: true,
       card: true,
       expiration: true,
-      cvv: true,
-      acknowledgement: true
+      cvv: true
     };
 
     const emailRegex = RegExp(/^(([^<>()\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
     const zipRegex = RegExp(/^[0-9]{5}(?:-[0-9]{4})?$/);
-    const expRegex = RegExp(/^(0[1-9]|1[0-2])\/[0-9]{4}$/);
 
     if (this.state.fullName.length < 5) {
       checkValidity.fullName = false;
@@ -118,7 +109,15 @@ class CheckoutForm extends React.Component {
       checkValidity.phone = false;
     }
 
-    if (this.state.state === 'Choose State...') {
+    if (this.state.address.length < 6) {
+      checkValidity.address = false;
+    }
+
+    if (this.state.city.length < 3) {
+      checkValidity.city = false;
+    }
+
+    if (this.state.state === '') {
       checkValidity.state = false;
     }
 
@@ -126,20 +125,20 @@ class CheckoutForm extends React.Component {
       checkValidity.zip = false;
     }
 
+    if (this.state.cardHolder.length < 5) {
+      checkValidity.cardHolder = false;
+    }
+
     if (this.state.card.length < 16) {
       checkValidity.card = false;
     }
 
-    if (!expRegex.test(this.state.expiration)) {
+    if (this.state.month === '' || this.state.year === '') {
       checkValidity.expiration = false;
     }
 
     if (this.state.cvv.length < 3) {
       checkValidity.cvv = false;
-    }
-
-    if (this.state.acknowledgement === false) {
-      checkValidity.acknowledgement = false;
     }
 
     if (Object.values(checkValidity).find(element => element === false) === undefined) {
@@ -150,7 +149,7 @@ class CheckoutForm extends React.Component {
         address: `${this.state.address} \n${this.state.address2} \n${this.state.city}, ${this.state.state} ${this.state.zip}`,
         cardHolder: this.state.cardHolder,
         card: this.state.card.split(' ').join().split('-').join(),
-        expiration: this.state.expiration,
+        expiration: `${this.state.month}/${this.state.year}`,
         cvv: this.state.cvv
       };
       this.props.checkout({ placeOrder });
@@ -167,45 +166,48 @@ class CheckoutForm extends React.Component {
     cart.forEach(product => {
       totalPrice = totalPrice + (product.price * product.quantity);
     });
-    // const total = '$' + ((totalPrice / 100).toFixed(2));
+    const total = '$' + ((totalPrice / 100).toFixed(2));
     return (
       <div className="container col-12 col-sm-10 col-md-8 col-lg-8 my-5">
         <div className="d-flex flex-wrap m-3">
+          <h2 className="text-secondary">Order Total: {total}</h2>
           <form className="col-12" onSubmit={this.handleSubmit}>
-            <h3 className="mb-3">Billing/Shipping</h3>
+            <h3 className="my-3">Billing/Shipping</h3>
             <div className="form-group">
               <label htmlFor="fullName">Full Name</label>
-              <input type="text" minLength="5" maxLength="65" className="form-control" id="fullName" value={this.state.fullName} name='fullName' onChange={this.handleChange} required />
+              <input type="text" maxLength="65" autoComplete="new-password" className={`form-control ${this.state.checkValidity.fullName ? null : 'is-invalid'}`} id="fullName" value={this.state.fullName} name='fullName' onChange={this.handleChange}/>
               <div className="invalid-feedback">Must be at least 5 characters.</div>
             </div>
             <div className="form-row">
               <div className="form-group col-md-6">
                 <label htmlFor="email">Email</label>
-                <input type="email" minLength="6" maxLength="254" className="form-control" id="email" value={this.state.email} name='email' onChange={this.handleChange} required />
+                <input type="text" maxLength="254" autoComplete="new-password" className={`form-control ${this.state.checkValidity.email ? null : 'is-invalid'}`} id="email" value={this.state.email} name='email' onChange={this.handleChange}/>
                 <div className="invalid-feedback">Please enter a valid email.</div>
               </div>
               <div className="form-group col-md-6">
                 <label htmlFor="phone">Phone Number</label>
-                <input type="tel" minLength="10" maxLength="11" className="form-control" id="phone" value={this.state.phone} name='phone' onChange={this.handleChange} placeholder="1234567890" required />
-                <div className="invalid-feedback">Please enter a valid phone number.</div>
+                <input type="tel" maxLength="11" autoComplete="new-password" className={`form-control ${this.state.checkValidity.phone ? null : 'is-invalid'}`} id="phone" value={this.state.phone} name='phone' onChange={this.handleChange} placeholder="1234567890"/>
+                <div className="invalid-feedback">Please enter a valid phone number (##########).</div>
               </div>
             </div>
             <div className="form-group">
               <label htmlFor="address">Address</label>
-              <input type="text" minLength="6" maxLength="42" className="form-control" id="address" value={this.state.address} name="address" placeholder="1234 Main St." onChange={this.handleChange} required />
+              <input type="text" maxLength="42" autoComplete="new-password" className={`form-control ${this.state.checkValidity.address ? null : 'is-invalid'}`} id="address" value={this.state.address} name="address" placeholder="1234 Main St." onChange={this.handleChange}/>
+              <div className="invalid-feedback">Please enter a valid address.</div>
             </div>
             <div className="form-group">
               <label htmlFor="address2">Address 2</label>
-              <input type="text" maxLength="42" className="form-control" id="address2" value={this.state.address2} name="address2" placeholder="Apartment, Studio, or Floor" onChange={this.handleChange} required />
+              <input type="text" maxLength="42" autoComplete="new-password" className="form-control" id="address2" value={this.state.address2} name="address2" placeholder="Apartment, Studio, or Floor" onChange={this.handleChange}/>
             </div>
             <div className="form-row">
               <div className="form-group col-md-6">
                 <label htmlFor="city">City</label>
-                <input type="text" minLength="3" maxLength="50" className="form-control" id="city" value={this.state.city} name="city" onChange={this.handleChange} required />
+                <input type="text" maxLength="50" autoComplete="new-password" className={`form-control ${this.state.checkValidity.city ? null : 'is-invalid'}`} id="city" value={this.state.city} name="city" onChange={this.handleChange}/>
+                <div className="invalid-feedback">Please enter a valid city.</div>
               </div>
               <div className="form-group col-md-4">
                 <label htmlFor="state">State</label>
-                <select id="state" className="form-control" value={this.state.state} name="state" onChange={this.handleChange} required >
+                <select id="state" className={`form-control ${this.state.checkValidity.state ? null : 'is-invalid'}`} value={this.state.state} name="state" onChange={this.handleChange}>
                   <option defaultValue >Choose State...</option>
                   <option value="AL">Alabama</option>
                   <option value="AK">Alaska</option>
@@ -259,45 +261,77 @@ class CheckoutForm extends React.Component {
                   <option value="WI">Wisconsin</option>
                   <option value="WY">Wyoming</option>
                 </select>
+                <div className="invalid-feedback">Please choose a state.</div>
               </div>
               <div className="form-group col-md-2">
                 <label htmlFor="zip">Zip</label>
-                <input type="text" minLength="5" maxLength="10" className="form-control" id="zip" value={this.state.zip} name="zip" onChange={this.handleChange} required />
+                <input type="text" maxLength="10" autoComplete="new-password" className={`form-control ${this.state.checkValidity.zip ? null : 'is-invalid'}`} id="zip" value={this.state.zip} name="zip" onChange={this.handleChange}/>
                 <div className="invalid-feedback">Please enter a valid zip code.</div>
               </div>
             </div>
-            <hr className="my-4" />
+            <hr className="my-4"/>
             <h3 className="mb-3">Payment</h3>
             <div className="form-row">
               <div className="form-group col-md-6">
                 <label htmlFor="cardHolder">Name on Card</label>
-                <input type="text" minLength="5" maxLength="65" className="form-control" id="cardHolder" value={this.state.cardHolder} name="cardHolder" onChange={this.handleChange} required />
+                <input type="text" maxLength="65" autoComplete="new-password" className={`form-control ${this.state.checkValidity.cardHolder ? null : 'is-invalid'}`} id="cardHolder" value={this.state.cardHolder} name="cardHolder" onChange={this.handleChange}/>
                 <div className="invalid-feedback">Must be at least 5 characters.</div>
               </div>
               <div className="form-group col-md-6">
                 <label htmlFor="cardNumber">Card Number</label>
-                <input type="text" minLength="16" maxLength="16" className="form-control" id="cardNumber" value={this.state.card} name="card" placeholder="0000 0000 0000 0000" onChange={this.handleChange} required />
+                <input type="text" maxLength="16" autoComplete="new-password" className={`form-control ${this.state.checkValidity.card ? null : 'is-invalid'}`} id="cardNumber" value={this.state.card} name="card" placeholder="0000 0000 0000 0000" onChange={this.handleChange}/>
                 <div className="invalid-feedback">Please enter a valid card number.</div>
               </div>
             </div>
             <div className="form-row">
               <div className="form-group col-md-4">
-                <label htmlFor="expiration">Expiration</label>
-                <div className="invalid-feedback">Please enter a valid expiration date.</div>
-                <input type="text" minLength="7" maxLength="7" className="form-control" id="expiration" value={this.state.expiration} name="expiration" placeholder="MM/YYYY" onChange={this.handleChange} required />
+                <label htmlFor="month">Month</label>
+                <select id="month" autoComplete="new-password" className={`form-control ${this.state.checkValidity.expiration ? null : 'is-invalid'}`} value={this.state.month} name="month" onChange={this.handleChange}>
+                  <option defaultValue>##</option>
+                  <option value="01">01</option>
+                  <option value="02">02</option>
+                  <option value="03">03</option>
+                  <option value="04">04</option>
+                  <option value="05">05</option>
+                  <option value="06">06</option>
+                  <option value="07">07</option>
+                  <option value="08">08</option>
+                  <option value="09">09</option>
+                  <option value="10">10</option>
+                  <option value="11">11</option>
+                  <option value="12">12</option>
+                </select>
+                <div className="invalid-feedback">Please choose a month.</div>
+              </div>
+              <div className="form-group col-md-4">
+                <label htmlFor="year">Year</label>
+                <select id="year" autoComplete="new-password" className={`form-control ${this.state.checkValidity.expiration ? null : 'is-invalid'}`} value={this.state.year} name="year" onChange={this.handleChange}>
+                  <option defaultValue>####</option>
+                  <option value="2020">2020</option>
+                  <option value="2021">2021</option>
+                  <option value="2022">2022</option>
+                  <option value="2023">2023</option>
+                  <option value="2024">2024</option>
+                  <option value="2025">2025</option>
+                  <option value="2026">2026</option>
+                  <option value="2027">2027</option>
+                  <option value="2028">2028</option>
+                  <option value="2029">2029</option>
+                </select>
+                <div className="invalid-feedback">Please choose a year.</div>
               </div>
               <div className="form-group col-md-4">
                 <label htmlFor="cvv">CVV</label>
-                <input type="text" minLength="3" maxLength="4" className="form-control" id="cvv" value={this.state.cvv} name="cvv" placeholder="###" onChange={this.handleChange} required />
+                <input type="text" maxLength="4" autoComplete="new-password" className={`form-control ${this.state.checkValidity.cvv ? null : 'is-invalid'}`} id="cvv" value={this.state.cvv} name="cvv" placeholder="###" onChange={this.handleChange}/>
                 <div className="invalid-feedback">Please enter a valid CVV.</div>
               </div>
             </div>
-            <hr className="my-4" />
+            <hr className="my-4"/>
             <div className="form-check">
-              <input className="form-check-input" type="checkbox" id="acknowledgement" required />
+              <input className="form-check-input" type="checkbox" id="acknowledgement" required/>
               <label className="form-check-label" htmlFor="acknowledgement">I acknowledge that this is a demo application, and the information above is not my genuine financial or personal information.</label>
             </div>
-            <hr className="my-4" />
+            <hr className="my-4"/>
             <button type="submit" className="btn btn-primary align-self-end">Submit</button>
           </form>
           <div className="container">
