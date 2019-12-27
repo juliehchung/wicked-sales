ad12f1652f42e23d59dcc5ad577c9c233a9b5e92
juliehchung <juliehchung96@gmail.com>
2019-12-27 21:29:42 +0000

    Completed confirmation page feature after user places an order

--- client/components/checkout-confirmation.jsx
+++ client/components/checkout-confirmation.jsx
@@ -7,31 +7,31 @@ function CheckoutConfirmation(props) {
   });
   const total = '$' + ((totalPrice / 100).toFixed(2));
   const confirmationList = props.purchased.map(product => {
+    let price = product.price;
+    price = '$' + ((price / 100).toFixed(2));
     return (
-      <div className="bg-white border rounded col-12 py-2 my-3" key={product.productId}>
+      <div className="bg-white border rounded col-md-8 py-4 my-3 mx-auto" key={product.productId}>
         <div className="row py-2">
-          <img src={product.image} alt={product.name} className="cart-item img-fluid col-md-4" />
-          <div className="col-md-8 d-flex flex-column align-self-center">
-            <h5>{product.name}</h5>
-            <h6 className="text-muted">{product.price}</h6>
-            <p>Quantity: {product.quantity}</p>
+          <img src={product.image} alt={product.name} className="cart-item img-fluid col-md-6" />
+          <div className="col-md-6 d-flex flex-column align-self-center">
+            <h4>{product.name}</h4>
+            <h5 className="text-muted">{price}</h5>
+            <h6>Quantity: {product.quantity}</h6>
           </div>
         </div>
       </div>
     );
   });
   return (
-    <div>
-      <h1>Your order has been placed!</h1>
-      <h2>Order Total: {total}</h2>
+    <div className="col-md-12 my-4">
+      <h1 className="col-md-8 mx-auto">Your order has been placed!</h1>
+      <h2 className="col-md-8 mx-auto text-muted">Order Total: {total}</h2>
       <div className="container">
         {confirmationList}
       </div>
-      <div className="container">
-        <div className="row d-flex justify-content-between align-items-center my-3">
-          <div className="back text-muted" onClick={() => props.viewData('catalog', {})}>
-            {'< Continue Shopping'}
-          </div>
+      <div className="col-md-8 mb-5 mx-auto">
+        <div className="back text-muted" onClick={() => props.viewData('catalog', {})}>
+          {'< Continue Shopping'}
         </div>
       </div>
     </div>
